421cf679e8be7a8cd040a576d7fa9cdb2643f266
Lucasfth <luha@itu.dk>
2023-08-31 11:43:11 +0200

    Finish exercise 1.1

--- assignment-1/Program.fs
+++ assignment-1/Program.fs
@@ -22,20 +22,20 @@ type expr =
     | CstI of int
     | Var of string
     | Prim of string * expr * expr
+    | If of expr * expr * expr
 
 
 let e1 = CstI 17
-
 let e2 = Prim("+", CstI 3, Var "a")
-
 let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a")
-
-let e4 = Prim("==", Prim("max", Var "a", Var "b"), Prim("min", Var "c", Var "baf")) // should eval to 0
-
-let e4_1 =
-    Prim("==", Prim("max", Var "a", Var "b"), Prim("min", Var "b", Var "baf")) // should eval to 1
-
-
+let equals_false = Prim("==", Var "b", CstI 20) // should eval to 0
+let equals_true = Prim("==", Var "b", CstI 111) // should eval to 1
+let max_first = Prim("max", Var "baf", CstI 665) // should eval to 666
+let max_second = Prim("max", Var "baf", CstI 667) // should eval to 667
+let min_first = Prim("min", Var "b", Var "baf") // Should eval to 111
+let min_second = Prim("min", Var "a", CstI 1) // Should eval to 1
+let if_true = If(Var "a", CstI 11, CstI 22) // should eval to 11
+let if_false = If(CstI 0, CstI 11, CstI 22) // should eval to 22
 
 (* Evaluation within an environment *)
 
@@ -43,27 +43,35 @@ let rec eval e (env: (string * int) list) : int =
     match e with
     | CstI i -> i
     | Var x -> lookup env x
-    | Prim("+", e1, e2) -> eval e1 env + eval e2 env
-    | Prim("*", e1, e2) -> eval e1 env * eval e2 env
-    | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim("==", e1, e2) -> if (eval e1 env) = (eval e2 env) then 1 else 0
-    | Prim("max", e1, e2) ->
-        if (eval e1 env) > (eval e2 env) then
-            eval e1 env
-        else
-            eval e2 env
-    | Prim("min", e1, e2) ->
-        if (eval e1 env) < (eval e2 env) then
-            eval e1 env
-        else
-            eval e2 env
-    | Prim _ -> failwith "unknown primitive"
+    | Prim(ope, e1, e2) ->
+        let i1 = eval e1 env
+        let i2 = eval e2 env
+
+        match ope with
+        | "+" -> i1 + i2
+        | "*" -> i1 * i2
+        | "-" -> i1 - i2
+        | "==" -> if i1 = i2 then 1 else 0
+        | "max" -> if i1 > i2 then i1 else i2
+        | "min" -> if i1 < i2 then i1 else i2
+        | _ -> failwith "unknown primitive"
+    | If(e1, e2, e3) -> if (eval e1 env) <> 0 then eval e2 env else eval e3 env
 
 let e1v = eval e1 env
 let e2v1 = eval e2 env
 let e2v2 = eval e2 [ ("a", 314) ]
 let e3v = eval e3 env
 
-let e4v = eval e4 env
+let e4v = eval equals_false env
+let e4_1v = eval equals_true env
 
-let e4_1v = eval e4_1 env
+let e5v = eval max_first env
+let e5_1 = eval max_second env
+
+let e6v = eval min_first env
+
+let e6_1 = eval min_second env
+
+let e7 = eval if_true env
+
+let e7_1 = eval if_false env
